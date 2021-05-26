276ef268023b7148a43f3fa15672d03dcc94e0b3
IngDevLuis <84766065+IngDevLuis@users.noreply.github.com>
2021-05-26 15:40:17 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,13 @@
-
+  
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBWVT7Jl7u9Jng16dBYUkxhpQDRAEDVjv4",
+    authDomain: "fbwebauth41-83bd2.firebaseapp.com",
+    projectId: "fbwebauth41-83bd2",
+    storageBucket: "fbwebauth41-83bd2.appspot.com",
+    messagingSenderId: "615458361154",
+    appId: "1:615458361154:web:8c2c23600cdaa4ffd6c2db",
+    measurementId: "G-BPQL23PQ1V"
+    });
